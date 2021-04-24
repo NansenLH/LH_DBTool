9ce3612ff1061c83d97eaebcf1a26fbad10f65e0
NansenLH <nansen_lu@163.com>
2021-04-24 22:15:17 +0800

    update

--- Example/LH_DBTool/LH_ViewController.m
+++ Example/LH_DBTool/LH_ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "LH_ViewController.h"
+#import "TestModel.h"
+
 
 @interface LH_ViewController ()
 
@@ -18,6 +20,41 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSString *docDir = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
+    
+    
+    NSMutableArray *arr = [NSMutableArray array];
+    for (int i = 0; i < 10; i++) {
+        TestModel *m1 = [[TestModel alloc] init];
+        m1.name = [NSString stringWithFormat:@"Name - %d", i];
+        m1.page = i;
+        m1.count = i+100;
+        [arr addObject:m1];
+    }
+    
+    NSLog(@"docDir = %@", docDir);
+    
+    [[LH_DBTool defaultTool] startWithDBPath:docDir dbName:@"abc.db"];
+    
+    [[LH_DBTool defaultTool] addObjectsInTransaction:arr];
+    
+    [[LH_DBTool defaultTool] startWithDBPath:docDir dbName:@"bcd.db"];
+    
+    NSMutableArray *arr2 = [NSMutableArray array];
+    for (int i = 20; i < 30; i++) {
+        TestModel *m1 = [[TestModel alloc] init];
+        m1.name = [NSString stringWithFormat:@"Name - %d", i];
+        m1.page = i;
+        m1.count = i+100;
+        [arr2 addObject:m1];
+    }
+    
+    
+    [[LH_DBTool defaultTool] addObjectsInTransaction:arr2];
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
