8555cf45deec0b68caaf98fc75c772c716d0a5ab
NansenLH <nansen_lu@163.com>
2021-04-28 18:24:05 +0800

    modification

--- LH_DBTool/Classes/LH_DBObjectProtocol.h
+++ LH_DBTool/Classes/LH_DBObjectProtocol.h
@@ -16,7 +16,6 @@ NS_ASSUME_NONNULL_BEGIN
 /// 如果返回多个字段.会默认创建多主键表.返回一个字段会创建单主键表,这里不能返回数组
 - (NSArray<NSString *> *)LH_Primarykey;
 
-@optional
 /// 用于查询的属性.必须是该 Model 的属性名. 而且属性只限于 NSString, int, NSInteger 这三种
 /// 不能和 LH_Primarykey 重复, 仅限用于查询的 condition 中使用
 - (NSArray<NSString *> *)LH_SearchKey;
